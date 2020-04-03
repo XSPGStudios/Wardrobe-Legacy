983a7e57b65e54fa376245ea6ed5a015d21fe6fb
UCMike <miky4321@live.it>
2020-04-03 23:11:30 +0200

    InsertDialog half

--- app/src/main/java/com/ucstudios/wardrobe/ItemVisualDialog.java
+++ app/src/main/java/com/ucstudios/wardrobe/ItemVisualDialog.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ItemVisualDialog extends Dialog {
 
@@ -17,6 +19,10 @@ public class ItemVisualDialog extends Dialog {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.item_visual_dialog);
+        Spinner spinner = findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(),R.array.currencies,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter1);
 
     }
 
