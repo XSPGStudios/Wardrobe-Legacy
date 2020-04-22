18da32fdd1e98dfbd36f0e7d59beef81039422ba
redarguireda <druvoilmonello123@gmail.com>
2020-04-22 23:36:20 +0200

    First steps for cancel notification La notifica si cancella ma ancora non ho trovato un modo per interrompere il laavaggio, working on that

--- app/src/main/java/com/ucstudios/wardrobe/NotificationReceiver.java
+++ app/src/main/java/com/ucstudios/wardrobe/NotificationReceiver.java
@@ -1,14 +1,23 @@
 package com.ucstudios.wardrobe;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
 public class NotificationReceiver extends BroadcastReceiver {
+    public static boolean cancelProgressBar() {
+                return true;
+    }
+
     @Override
-    public void onReceive(Context context, Intent intent) {
-        String message = intent.getStringExtra("toastMessage");
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    public void onReceive(final Context context, final Intent intent) {
+        final String message = intent.getStringExtra("cancel");
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                    notificationManager.cancel(2);
+                        cancelProgressBar();
     }
 }
