fa07dba8f52d359424ea5f71a926f458522db29d
redarguireda <druvoilmonello123@gmail.com>
2020-04-27 01:53:41 +0200

    Home fragment completed

--- app/src/main/java/com/ucstudios/wardrobe/HomeFragment.java
+++ app/src/main/java/com/ucstudios/wardrobe/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.ucstudios.wardrobe;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.CalendarView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -23,6 +28,8 @@ public class HomeFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+    ImageView imageViewTwitter;
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -50,13 +57,32 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+            imageViewTwitter = view.findViewById(R.id.twitter);
+                TextView tv = view.findViewById(R.id.textViewTwitterLink);
+        SpannableString content = new SpannableString("Follow us on twitter!");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        tv.setText(content);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.twitter.com/XSPGStudios"));
+                startActivity(intent);
+            }
+        });
+        return view;
+
     }
+
     }
 
 
